cd7cda1f0ef85306fde32b31592884540bf572b3
Galabin Vasilev <bootnew@gmail.com>
2015-04-21 14:38:24 +0300

    Update convo_fetch.php

--- apps/convo_fetch.php
+++ apps/convo_fetch.php
@@ -3,7 +3,7 @@
 
 	$room = $_POST['roomName'];
 
-	$result = $db->query("SELECT messageID FROM messages WHERE messages.roomName='$room'");	
+	$result = $db->query("SELECT `messageID` FROM `messages` WHERE `messages.roomName`='$room'");	
 	while(($row = $result->fetch()) != false){
 		$msg = new Message($row['messageID']);
 		$msg->fetch_build();
